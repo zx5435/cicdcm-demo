71303162328af253fc901ff55b02d32c009f692b
赵煜杰 <kayl.zhao@appcoachs.com>
2018-01-11 18:34:26 +0800

    123

--- 1.php
+++ 1.php
@@ -1,6 +1,9 @@
 <?php
 
+$gitid = file_get_contents('.git/refs/heads/master');
+
 echo json_encode([
+    'gitid'  => trim($gitid),
     'hostname'  => gethostname(),
     'microtime' => microtime(true),
 ], JSON_PRETTY_PRINT);
