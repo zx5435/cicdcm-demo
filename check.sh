d5db65a906c00676554d3ee61b0cd26080700d37
赵煜杰 <kayl.zhao@appcoachs.com>
2018-01-22 11:13:09 +0800

    a

--- check.sh
+++ check.sh
@@ -3,9 +3,12 @@
 # tee check.log <<-'EOF'
 # EOF
 
+echo 'Using `sh ./check.sh localhost:1212/1.php`'
+echo 'url: '$1
+
 while true;do
 	# curl -sS localhost:1212/1.html >> check.log 2>&1
 	# curl -s localhost:1212/1.html -i | grep -e 'Date:' -e 'X-HOSTNAME:' -e 'html:' >> check.log
-	curl -sS localhost:1212/1.php >> check.log 2>&1
+	curl -sS $1 >> check.log 2>&1
 done;
 
